04a6af9eb4e5dc6ea5cf3a2fd1c4d0dc77fa7ef8
Mahiballah <145888808+mahibullah11@users.noreply.github.com>
2024-09-06 22:33:42 +0500

    done with home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav className='flex justify-between px-5 py-2 h-auto sm:h-20 bg-gray-500 sm:flex-row flex-col'>
+    <nav className='flex justify-between px-5 py-2 h-auto sm:h-20 bg-blue-400 sm:flex-row flex-col'>
   <div className='flex justify-center items-center gap-x-2 mb-2 sm:mb-0'>
-    {/* <img className='w-[60px] rounded' src="https://images.unsplash.com/photo-1430163393927-3dab9af7ea38?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZCUyMGluY29uc3xlbnwwfHwwfHx8MA%3D%3D" alt="Logo" /> */}
-    <h2 className='font-bold text-lg text-white'>WOW FOOD</h2>
+    <img className='w-[60px] rounded' src="https://img.icons8.com/?size=100&id=ZF7hWVW0tEYV&format=png&color=000000" alt="Logo" />
+    <h2 className='font-bold text-lg text-white'>WOW BURGER</h2>
   </div>
   <ul className='flex list-none gap-x-5 justify-center items-center'>
     <li className='text-white hover:text-pink-400 hover:cursor-pointer font-semibold'>Home</li>
